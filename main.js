e3327e37318241ff35977e7fc2220a363890ed75
Dom\Domz <dv2015-0463@educationgroup.co.za>
2018-08-04 10:21:18 +0200

    Updated Js

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
-window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    
-    if(!audio) return;//stop the function
-    audio.currentTime = 0; //Rewind the audio
-    audio.play();
-    key.classList.add('playing'); // add css class for different styling
-});
-
-
-function removeTransition(e){
-    if(e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
-}
-
-const keys = document.querySelectorAll(".key");
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+var Controller = (function() {
+    function playSound(e) {
+      const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+      const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  
+      if (!audio) return;
+      audio.currentTime = 0;
+      audio.play();
+      key.classList.add("playing");
+    }
+  
+    function removeTransition(e) {
+      if (e.propertyName !== "transform") return;
+      this.classList.remove("playing");
+    }
+  
+    return {
+      playSound: playSound,
+      removeTransition: removeTransition
+    };
+  })();
+  
+  const keys = document.querySelectorAll(".key");
+  keys.forEach(key =>key.addEventListener("transitionend", Controller.removeTransition));
+  
+  window.addEventListener("keydown", Controller.playSound);
+  
